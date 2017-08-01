fc393bd638dedf3e447d7c88f1e449977e297235
Joe Clarke <jclarke@cisco.com>
2017-08-01 11:37:13 -0400

    Clean up the HTML.

--- blog.php
+++ blog.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
-<html lang="en"><head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Blog | YANG Catalog</title>
-	<script defer="defer" src="Blog%20%7C%20YANG%20Catalog_files/beacon.js"></script><script type="text/javascript">
+<html lang="en">
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Blog | YANG Catalog</title>
+	<script defer="defer" src="Blog%20%7C%20YANG%20Catalog_files/beacon.js"></script>
+	<script type="text/javascript">
 		/* <![CDATA[ */
 		function addLoadEvent(func) {
 			var oldonload = window.onload;
 			if (typeof window.onload != 'function') {
 				window.onload = func;
 			} else {
-				window.onload = function () {
+				window.onload = function() {
 					oldonload();
 					func();
 				}
@@ -19,94 +22,122 @@
 		}
 		/* ]]> */
 	</script>
-<link rel="stylesheet" id="all-css-0-1" href="css/a.css" type="text/css" media="all">
-<link rel="stylesheet" id="edin-pt-sans-css" href="css/css_002.css" type="text/css" media="all">
-<link rel="stylesheet" id="edin-pt-serif-css" href="css/css.css" type="text/css" media="all">
-<link rel="stylesheet" id="edin-pt-mono-css" href="css/css_003.css" type="text/css" media="all">
-<link rel="stylesheet" id="all-css-6-1" href="css/a_002.css" type="text/css" media="all">
-<style id="edin-style-inline-css" type="text/css">
-.hero.with-featured-image { background-image: url(https:/yangcatalog.org/img/yang-explorer.jpg?w=1230&#038;h=831&#038;crop=1); }
-</style>
-<link rel="stylesheet" id="print-css-7-1" href="css/global-print.css" type="text/css" media="print">
-<link rel="stylesheet" id="all-css-8-1" href="css/a_003.css" type="text/css" media="all">
-<script type="text/javascript" src="js/a.js"></script>
+	<link rel="stylesheet" id="all-css-0-1" href="css/a.css" type="text/css" media="all">
+	<link rel="stylesheet" id="edin-pt-sans-css" href="css/css_002.css" type="text/css" media="all">
+	<link rel="stylesheet" id="edin-pt-serif-css" href="css/css.css" type="text/css" media="all">
+	<link rel="stylesheet" id="edin-pt-mono-css" href="css/css_003.css" type="text/css" media="all">
+	<link rel="stylesheet" id="all-css-6-1" href="css/a_002.css" type="text/css" media="all">
+	<style id="edin-style-inline-css" type="text/css">
+		.hero.with-featured-image {
+			background-image: url(https:/yangcatalog.org/img/yang-explorer.jpg?w=1230&#038;h=831&#038;crop=1);
+		}
+	</style>
+	<link rel="stylesheet" id="print-css-7-1" href="css/global-print.css" type="text/css" media="print">
+	<link rel="stylesheet" id="all-css-8-1" href="css/a_003.css" type="text/css" media="all">
+	<script type="text/javascript" src="js/a.js"></script>
 
-<meta name="theme-color" content="#ffffff">
-<meta name="application-name" content="YANG Catalog"><meta name="msapplication-window" content="width=device-width;height=device-height"><meta name="title" content="YANG Catalog">
-<style type="text/css" id="syntaxhighlighteranchor"></style>
-<link rel="stylesheet" type="text/css" id="gravatar-card-css" href="css/hovercard.css"><link rel="stylesheet" type="text/css" id="gravatar-card-services-css" href="css/services.css"></head>
+	<meta name="theme-color" content="#ffffff">
+	<meta name="application-name" content="YANG Catalog">
+	<meta name="msapplication-window" content="width=device-width;height=device-height">
+	<meta name="title" content="YANG Catalog">
+	<style type="text/css" id="syntaxhighlighteranchor"></style>
+	<link rel="stylesheet" type="text/css" id="gravatar-card-css" href="css/hovercard.css">
+	<link rel="stylesheet" type="text/css" id="gravatar-card-services-css" href="css/services.css">
+</head>
 
 <body class="blog mp6 customizer-styles-applied has-header-search navigation-default sidebar-right has-site-logo highlander-enabled highlander-light small-screen medium-screen large-screen">
-<div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content">Skip to content</a>
+	<div id="page" class="hfeed site">
+		<a class="skip-link screen-reader-text" href="#content">Skip to content</a>
 
-	<header id="masthead" class="site-header" role="banner">
-				<a href="https://yangcatalog.org" class="header-image" rel="home">
+		<header id="masthead" class="site-header" role="banner">
+			<a href="https://yangcatalog.org" class="header-image" rel="home">
 			<img src="img/blur.jpg" alt="" scale="0" width="1230" height="100">
-		</a><!-- .header-image -->
-		
-		
-		<div class="header-wrapper clear">
-			<div class="site-branding" style="max-width: 904px;">
-				<a href="https://yangcatalog.org" class="site-logo-link" rel="home"><img src="img/all-yang-modules1.jpg" class="site-logo attachment-edin-logo" alt="" data-size="edin-logo"  width="219" height="192"></a>						<p class="site-title"><a href="https://yangcatalog.org" rel="home">YANG Catalog</a></p>
-								</div><!-- .site-branding -->
+		</a>
+			<!-- .header-image -->
 
-					</div><!-- .header-wrapper -->
-	</header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+			<div class="header-wrapper clear">
+				<div class="site-branding" style="max-width: 904px;">
+					<a href="https://yangcatalog.org" class="site-logo-link" rel="home"><img src="img/all-yang-modules1.jpg" class="site-logo attachment-edin-logo" alt="" data-size="edin-logo"  width="219" height="192"></a>
+					<p class="site-title"><a href="https://yangcatalog.org" rel="home">YANG Catalog</a></p>
+				</div>
+				<!-- .site-branding -->
 
-	<div class="content-wrapper clear">
+			</div>
+			<!-- .header-wrapper -->
+		</header>
+		<!-- #masthead -->
 
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main" role="main">
+		<div id="content" class="site-content">
 
-				
-											<header>
+			<div class="content-wrapper clear">
+
+				<div id="primary" class="content-area">
+					<main id="main" class="site-main" role="main">
+
+
+						<header>
 							<h1 class="page-title screen-reader-text">Blog</h1>
 						</header>
-					
-										
-						
-<article id="post-6" class="post-6 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized">
-	<header class="entry-header">
-		<h2 class="entry-title"><a href="/blog.php" rel="bookmark">YANG Catalog Latest Development (IETF 98&nbsp;Hackathon)</a></h2>
-				<div class="entry-meta">
-			<span class="posted-on">Posted on <a href="/blog.php" rel="bookmark"><time class="entry-date published" datetime="2017-04-16T08:34:30+00:00">April 16, 2017</time><time class="updated" datetime="2017-04-16T12:24:47+00:00">April 16, 2017</time></a></span><span class="byline"> by <span class="author vcard">Benoit Claise</span></span>		</div><!-- .entry-meta -->
-			</header><!-- .entry-header -->
 
-	
-	<a class="post-thumbnail" href="/blog.php">
+
+
+						<article id="post-6" class="post-6 post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized">
+							<header class="entry-header">
+								<h2 class="entry-title"><a href="/blog.php" rel="bookmark">YANG Catalog Latest Development (IETF 98&nbsp;Hackathon)</a></h2>
+								<div class="entry-meta">
+									<span class="posted-on">Posted on <a href="/blog.php" rel="bookmark"><time class="entry-date published" datetime="2017-04-16T08:34:30+00:00">April 16, 2017</time><time class="updated" datetime="2017-04-16T12:24:47+00:00">April 16, 2017</time></a></span>
+									<span
+									  class="byline"> by <span class="author vcard">Benoit Claise</span></span>
+								</div>
+								<!-- .entry-meta -->
+							</header>
+							<!-- .entry-header -->
+
+
+							<a class="post-thumbnail" href="/blog.php">
 		<img src="img/yang-explorer.jpg" class="attachment-edin-featured-image size-edin-featured-image wp-post-image" alt="" width="648" height="334">	</a>
 
 
-	<div class="entry-content">
-		<p><a href="http://www.claise.be/2017/04/yang-catalog-latest-development-ietf-98-hackathon/" rel="nofollow">http://www.claise.be/2017/04/yang-catalog-latest-development-ietf-98-hackathon/</a></p>
-	</div><!-- .entry-content -->
+							<div class="entry-content">
+								<p><a href="http://www.claise.be/2017/04/yang-catalog-latest-development-ietf-98-hackathon/" rel="nofollow">http://www.claise.be/2017/04/yang-catalog-latest-development-ietf-98-hackathon/</a></p>
+							</div>
+							<!-- .entry-content -->
 
-</article><!-- #post-## -->
-
-					
-					
-				
-			</main><!-- #main -->
-		</div><!-- #primary -->
-	
-</div><!-- .content-wrapper -->
-
-	</div><!-- #content -->
-
-	
-
-	<?php include_once "inc.footer.php"; ?>
+						</article>
+						<!-- #post-## -->
 
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="footer-wrapper clear">
-			<div class="site-info">
-				
-							</div><!-- .site-info -->
-					</div><!-- .footer-wrapper -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-</body></html>
+
+
+					</main>
+					<!-- #main -->
+				</div>
+				<!-- #primary -->
+
+			</div>
+			<!-- .content-wrapper -->
+
+		</div>
+		<!-- #content -->
+
+
+
+		<?php include_once "inc.footer.php"; ?>
+
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="footer-wrapper clear">
+				<div class="site-info">
+
+				</div>
+				<!-- .site-info -->
+			</div>
+			<!-- .footer-wrapper -->
+		</footer>
+		<!-- #colophon -->
+	</div>
+	<!-- #page -->
+</body>
+
+</html>
