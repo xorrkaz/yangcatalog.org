3eec593e3daa7a3f414232cf9f3817105a2b9f75
miroslavKovacPantheon <miroslav.kovac@pantheon.tech>
2018-01-22 17:50:48 +0100

    Upate title

--- create.php
+++ create.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Yang-catalog register</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
